c9193b25b11d4457d8509bafcd085384a445cfa3
Kevin Leary <info@kevinleary.net>
2015-06-06 21:40:27 -0400

    initial static node server setup

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Static HTTP Server
+ *
+ * Create a static file server instance to serve files
+ * and folder in the './public' folder
+ */
+'use strict';
+
+// modules
+var server = require('node-static');
+var port = 8080;
+var http = require('http');
+
+// config
+var file = new server.Server('./public', {
+	cache: 3600,
+	gzip: true
+});
+
+// serve
+http.createServer(function (request, response) {
+	request.addListener('end', function () {
+		file.serve(request, response);
+	}).resume();
+}).listen(port);
